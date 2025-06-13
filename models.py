83110f77411003b03a1af9818d5f79836e113aaa
Atizaz <atizaz.development@gmail.com>
2025-06-13 11:14:30 +0500

    work on user model update issue

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=True)
 
     def set_password(self, password):
